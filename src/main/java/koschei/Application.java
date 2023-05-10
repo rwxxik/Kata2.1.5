12a45006daeba5738a96abe90dd0425744afd164
a <rwxxik@gmail.com>
2023-05-10 20:28:38 +0300

    task complete

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -11,6 +11,5 @@ public class Application {
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
-        //test
     }
 }
